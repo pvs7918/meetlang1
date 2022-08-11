57840d1fbeadc2fda7b1098778831f4f4c63e177
Pavel <p_v_s@mail.ru>
2022-08-12 00:07:13 +0700

    Задача 2 исправлена

--- meetlang_task2/Program.cs
+++ meetlang_task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+Выполнил Сергиенко П.В. 11.08.2022г.
+*/
+
+int res;
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else
+{
+    if (num2 > num1)
+    {
+        Console.WriteLine($"max = {num2}");
+    }
+    else
+    {
+        Console.WriteLine("Числа равны!");
+    }
+}
