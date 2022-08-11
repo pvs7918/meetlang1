1bda22e8e4e3dea2123d3568ba69dfe6b28b520a
Pavel <p_v_s@mail.ru>
2022-08-12 00:40:13 +0700

    Add task 6

---
+++ meetlang_task6/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+int numb;       //исходное число
+int remainder;  //остаток от деления
+
+Console.WriteLine("Введите число: ");
+numb = int.Parse(Console.ReadLine()!);
+
+remainder = numb % 2;
+
+Console.WriteLine("Является ли введённое число чётным?");    
+Console.Write(" -> ");    
+ 
+if (remainder == 0)
+{
+    Console.WriteLine("да");    
+}
+else
+{
+    Console.WriteLine("нет");    
+}
